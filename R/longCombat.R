998728c96e2521a7ab438d3aad3f81be3af7a001
Joanne Beer <joannecbeer@gmail.com>
2020-07-01 17:27:26 -0400

    update error message for missing data

--- R/longCombat.R
+++ R/longCombat.R
@@ -56,7 +56,7 @@ longCombat <- function(idvar, batchvar, features,
   
   # check for missing data 
   if (sum(is.na(data)) > 0) {
-    stop('Missing data in the data frame. Either impute the missing values, remove those row(s), or remove column(s) with missing values if that variable is not in the model.')
+    stop('Missing data in the data frame. Before running longCombat either impute the missing values, remove those rows, or remove columns with missing values if that variable is not in the model.')
   }
   # make batch a factor if not already
   batch <- as.factor(data[,batchvar])
