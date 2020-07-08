4824ac8df014949913b77a83a0a3a98b411049e8
Joanne Beer <joannecbeer@gmail.com>
2020-07-08 12:42:40 -0400

    update trajPlot function

--- R/trajPlot.R
+++ R/trajPlot.R
@@ -1,40 +1,39 @@
 #' Plot Individual Subject Trajectories
 #' 
 #' \code{trajPlot} will plot individual subject trajectories for multi-batch longitudinal data. Each line represents a trajectory of observations of a single feature (e.g., left fusiform cortical thickness) over time for an individual subject. Plotting points are coded by batch. Data should be in "long" format.
-#' @param idvar name of ID variable (character string).
-#' @param timevar name of variable that distinguishes within-subject repeated measures, e.g., time, age, or visit (character string). Will be plotted along x-axis.
-#' @param feature name of the feature variable (character string) to be plotted over time, or the numeric index of the corresponding column. Will be plotted along y-axis.
-#' @param batchvar name of the batch/site/scanner variable (character string). Will determine the shape of plotting points.
-#' @param point.shape vector that encodes the point shape for the batch variable levels. Should be in the same order as \code{levels(data[subset, batchvar])}. See the \code{pch} argument in \code{?points}. Default cycles through \code{pch=c(1:25, 0)} by batch level.
-#' @param point.col vector that encodes colors of the point for each observation (character string of color names or hexadecimal codes). Length should be equal to number of unique observations included (\code{nrow(data[subset,])}) and in the same order as in the data frame. Default is \code{'black'} for all.
-#' @param line.col vector that encodes colors of the line for each subject (character string of color names or hexadecimal codes). Length should be equal to number of unique subjects included (\code{length(unique(data[subset,idvar]))}) and in the same order as in the data frame. Default is \code{'black'} for all.
-#' @param xlabel x-axis label, default is \code{'time'} (character string).
-#' @param ylabel y-axis label, default is \code{'feature'} (character string).
-#' @param title main title for the plot, default is no title (character string).
-#' @param xlimits two dimensional numeric vector giving minimum and maximum x-axis values.
-#' @param ylimits two dimensional numeric vector giving minimum and maximum y-axis values.
+#' @param idvar character string that specifies name of ID variable. ID variable can be factor, numeric, or character. 
+#' @param timevar character string that specifies name of numeric variable that distinguishes within-subject repeated measures, e.g., time, age, or visit. Will be plotted along x-axis.
+#' @param feature character string that specifies name of the numeric feature variable to be plotted over time, or the numeric index of the corresponding column. Will be plotted along y-axis.
+#' @param batchvar character string that specifies name of the batch variable. Batch variable should be a factor. Will determine the shape of plotting points.
+#' @param point.shape [optional] numeric vector that encodes the point shape for the batch variable levels. If not using default setting, length should be equal to number of unique observations included (\code{nrow(data)}) and in the same order as in the data frame. See the \code{pch} argument in \code{\link[graphics]{points}}. Default cycles through \code{pch=c(1:25, 0)} by batch level.
+#' @param point.col [optional] vector that encodes colors of the point for each observation (character string of color names or hexadecimal codes). Length should be equal to number of unique observations included (\code{nrow(data)}) and in the same order as in the data frame. Default is \code{'black'} for all.
+#' @param line.col [optional] vector that encodes colors of the line for each subject (character string of color names or hexadecimal codes). Length should be equal to number of unique subjects included (\code{length(unique(data[,idvar]))}) and in the same order as in the data frame. Default is \code{'black'} for all.
+#' @param line.type [optional] numeric or character vector that encodes line type for each subject. Length should be equal to number of unique subjects included (\code{length(unique(data[,idvar]))}) and in the same order as in the data frame. See the \code{lty} argument in \code{\link[graphics]{par}}. Default is \code{'solid'} for all.
+#' @param xlabel x-axis label (character string), default is \code{'time'}.
+#' @param ylabel y-axis label (character string), default is \code{'feature'}.
+#' @param title main title for the plot (character string), default is no title.
+#' @param xlimits two dimensional numeric vector giving minimum and maximum x-axis values. Default is minimum and maximum time variable values.
+#' @param ylimits two dimensional numeric vector giving minimum and maximum y-axis values. Default is minimum and maximum feature variable values.
+#' @param margins numeric vector of 4 values specifying margins in the order (bottom, left, top, right).
 #' @param data name of the data frame that contains the variables above. Rows are different observations (subject/timepoints), columns are different variables.
-#' @param subset logical or numeric 0/1 vector the same length as \code{nrow(data)} denoting which observations (i.e., rows) to plot. Default is to include all rows.
 #' @param verbose prints messages (logical \code{TRUE} or \code{FALSE}). Default is \code{TRUE}.
-#' @param ... other graphical parameter arguments passed to \code{par()}.
+#' @param ... other graphical parameter arguments passed to \code{\link[graphics]{par}}.
 #' @return Creates a plot.
 
 trajPlot <- function(idvar, timevar, feature, batchvar, 
-                     point.shape=NULL, point.col=NULL, line.col=NULL, 
+                     point.shape=NULL, point.col=NULL, 
+                     line.col=NULL, line.type=NULL,
                      xlabel='time', ylabel='feature', title='', 
                      xlimits=NULL, ylimits=NULL, margins=c(5, 5, 3, 1),
                      data, subset=rep(TRUE, nrow(data)), verbose=TRUE, ...){
   
-  # get data subset
-  if (is.null(subset)) subset <- rep(TRUE, nrow(data))
-  data.subset <- data[subset,]
   # number of unique subjects 
-  subjectIDs <- unique(data.subset[,idvar])
+  subjectIDs <- unique(data[,idvar])
   n <- length(subjectIDs)
   # number of observations
-  L <- nrow(data.subset)
+  L <- nrow(data)
   # make batch a factor if not already
-  batch <- droplevels(as.factor(data.subset[,batchvar]))
+  batch <- droplevels(as.factor(data[,batchvar]))
   # number of batches
   m <- nlevels(batch)
   # print messages
@@ -44,18 +43,19 @@ trajPlot <- function(idvar, timevar, feature, batchvar,
 
   # set limits for x-axis (time) and y-axis (feature)
   if (is.null(xlimits)){
-    xlimits <- c(min(data.subset[,timevar]), max(data.subset[,timevar]))
+    xlimits <- c(min(data[,timevar]), max(data[,timevar]))
   }
   if (is.null(ylimits)){
-    ylimits <- c(min(data.subset[,feature]), max(data.subset[,feature]))
+    ylimits <- c(min(data[,feature]), max(data[,feature]))
   }
   
   # make point shape and color vectors
-  if (is.null(point.shape)) point.shape <- as.numeric(data.subset[,batchvar]) %% 26
+  if (is.null(point.shape)) point.shape <- as.numeric(data[,batchvar]) %% 26
   if (is.null(point.col)) point.col <- rep('black', L)
   if (is.null(line.col)) line.col <- rep('black', n)
-  data.subset$point.shape <- point.shape
-  data.subset$point.col <- point.col
+  if (is.null(line.type)) line.type <- rep('solid', n)
+  data$point.shape <- point.shape
+  data$point.col <- point.col
   
   ##############################
   # make plot
@@ -63,20 +63,20 @@ trajPlot <- function(idvar, timevar, feature, batchvar,
   par(mar=margins, ...)
   # plot first subject trajectory
   # get the data
-  data.subject <- data.subset[data.subset[,idvar]==subjectIDs[1],]
+  data.subject <- data[data[,idvar]==subjectIDs[1],]
   # sort by time point
   data.subject <- data.subject[order(data.subject[,timevar]),]
   # plot line
-  plot(data.subject[,timevar], data.subject[,feature], col=line.col[1], type='l', xlim=xlimits, ylim=ylimits, xlab=xlabel, ylab=ylabel, main=title, las=1)
+  plot(data.subject[,timevar], data.subject[,feature], col=line.col[1], lty=line.type[1], type='l', xlim=xlimits, ylim=ylimits, xlab=xlabel, ylab=ylabel, main=title, las=1)
   # plot points
   points(data.subject[,timevar], data.subject[,feature], pch=data.subject$point.shape, col=data.subject$point.col)
   for(i in 2:n){ # begin loop over remaining subjects
     # get the data
-    data.subject <- data.subset[data.subset[,idvar]==subjectIDs[i],]
+    data.subject <- data[data[,idvar]==subjectIDs[i],]
     # sort by time point
     data.subject <- data.subject[order(data.subject[,timevar]),]
     # plot line
-    lines(data.subject[,timevar], data.subject[,feature], col=line.col[i])
+    lines(data.subject[,timevar], data.subject[,feature], col=line.col[i], lty=line.type[i])
     # plot points
     points(data.subject[,timevar], data.subject[,feature], pch=data.subject$point.shape, col=data.subject$point.col)
   } # end loop over remaining subjects
